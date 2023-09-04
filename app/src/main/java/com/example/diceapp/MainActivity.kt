b718ca9979a8b2bcde8dff3d1d616c80c6997d66
Joao Pedro <joaochecco@gmail.com>
2023-09-04 20:33:46 -0300

    Logica simples e imagens dos lados do dado

--- app/src/main/java/com/example/diceapp/MainActivity.kt
+++ app/src/main/java/com/example/diceapp/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.diceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var imageView: ImageView
+    lateinit var button: Button
+    var dice: Dice = Dice()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.imageView)
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "DADO ROLADO", Toast.LENGTH_SHORT).show()
+            when (dice.roll()) {
+                1 -> imageView.setImageResource(R.drawable.dice_1)
+                2 -> imageView.setImageResource(R.drawable.dice_2)
+                3 -> imageView.setImageResource(R.drawable.dice_3)
+                4 -> imageView.setImageResource(R.drawable.dice_4)
+                5 -> imageView.setImageResource(R.drawable.dice_5)
+                6 -> imageView.setImageResource(R.drawable.dice_6)
+            }
+        }
     }
 }
